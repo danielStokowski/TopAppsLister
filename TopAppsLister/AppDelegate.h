4dfd49d51e59b3272287d285ed295a119e40ceea
Daniel Stokowski <d.stokowski@nomtek.com>
2014-04-22 14:20:37 +0200

    Initial Commit

---
+++ TopAppsLister/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TopAppsLister
+//
+//  Created by Daniel Stokowski on 22.04.2014.
+//  Copyright (c) 2014 BLTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TopAppsListerTests/TopAppsListerTests.m
@@ -0,0 +1,34 @@
+//
+//  TopAppsListerTests.m
+//  TopAppsListerTests
+//
+//  Created by Daniel Stokowski on 22.04.2014.
+//  Copyright (c) 2014 BLTest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TopAppsListerTests : XCTestCase
+
+@end
+
+@implementation TopAppsListerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
